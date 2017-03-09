def2667982fd126e6744153413959308b6b99364
Victor Hugo Candido de Oliveira <victorhcandido@gmail.com>
2017-03-09 19:47:36 -0300

    Output of all columns

--- order_csv.py
+++ order_csv.py
@@ -1,6 +1,18 @@
 import pandas as pd
 
+# Load and sort the values
 df = pd.read_csv('fx_events.csv')
 df.sort_values( by = ['date', 'time', 'currency'], inplace = True )
+
+# Get column names
+# slower -> header = list(df)
+header = df.columns.values.tolist()
+
+# Choose columns to appear first
 cols_order = ['date','weekday','time','currency','impact','event','eventid','actual','forecast','previous']
+for col in header:
+    if col not in cols_order:
+        cols_order.append(col)
+
+# Write data in a CSV file
 df[cols_order].to_csv('fx_events.csv', index = False)
